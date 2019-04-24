1b04cffde6bd5034f69b36d56bb1a4bc72593eb5
Lawrence <lawrencekkyalo@gmail.com>
2019-04-24 17:10:03 +0300

    .

--- AngularClient/src/app/app.component.ts
+++ AngularClient/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularClient';
+  public title = 'My Angular App';
+  public isAuthenticated: boolean;
+
+  constructor() {
+    this.isAuthenticated = false;
+  }
+
+  login() {
+  }
+
+  logout() {
+  }
 }
