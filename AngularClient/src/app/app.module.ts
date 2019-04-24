df4a901590362668b2134563776f3f303973f899
Lawrence <lawrencekkyalo@gmail.com>
2019-04-24 17:13:20 +0300

    .

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -16,10 +16,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     AppRoutingModule,
