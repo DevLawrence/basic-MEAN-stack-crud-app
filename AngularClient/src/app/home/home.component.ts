d5366a9e711b44c6976ebfa863b13f68d6dfb991
Lawrence <lawrencekkyalo@gmail.com>
2019-04-25 12:20:34 +0300

    .

--- AngularClient/src/app/home/home.component.ts
+++ AngularClient/src/app/home/home.component.ts
@@ -1,15 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+<div class="hero">
+  <div>
+    <h1>Hello World</h1>
+    <p class="lead">This is the homepage of your Angular app</p>
+  </div>
+</div>
